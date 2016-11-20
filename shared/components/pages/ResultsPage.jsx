b267f9449652a49c0aba780eba9fd57f2b5e6e49
vladimirkolodyazhniy <workmcvan007@gmail.com>
2016-11-20 18:29:56 +0200

    Resolve #8 Add pie chart to view results (#16)
    * hw12_2 Resolve #8  Add pie chart to view results
    
    * hw12_2 Resolve #8  Add pie chart to view results ( update color palette )
    
    * hw12_2 Resolve #8  Add pie chart to view results ( fix one line result text, code optimization )

--- shared/components/pages/ResultsPage.jsx
+++ shared/components/pages/ResultsPage.jsx
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 
 import { sprintf } from '../../utils';
 
+import Chart from 'react-chartjs';
+
 import Button from '../Button.jsx';
 
 if (process.env.BROWSER) {
     require('./ResultsPage.less');
 }
+
+const CHART_PIE_COLOR_PALETTE = [
+    '#f44336', '#e91e63', '#9c27b0',
+    '#673ab7', '#3f51b5', '#2196f3',
+    '#03a9f4', '#00bcd4', '#009688',
+    '#4caf50', '#8bc34a', '#cddc39'
+];
 
 export default class App extends Component {
     static contextTypes = { i18n: React.PropTypes.object };
@@ -20,16 +29,33 @@ export default class App extends Component {
             return <div>{l('Loading...')}</div>;
         }
 
+        const pollData = poll.options.map((option, idx) => ({
+            value: poll.results[idx],
+            color: CHART_PIE_COLOR_PALETTE[idx],
+            label: option
+        }));
+
         return (
             <div className='ResultsPage'>
                 <h2 className='ResultsPage__question'>
                     {poll.question}
                 </h2>
+
+                <Chart.Pie
+                    data={pollData}
+                    width='300'
+                    height='300'
+                />
+
                 <ul className='ResultsPage__options'>
                     {
                         poll.options.map((option, idx) =>
                             <li className='ResultsPage__option' key={idx}>
                                 <div className='ResultsPage__option-text'>
+                                    <i
+                                        style={{ backgroundColor: CHART_PIE_COLOR_PALETTE[idx] }}
+                                        className='ResultsPage__option-color'
+                                    />
                                     {option}
                                 </div>
                                 <div className='ResultsPage__option-result'>
